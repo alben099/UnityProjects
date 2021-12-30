393ca1aa9b95b66791936db77f40cb3eab80c8c7
Alben Trang <96761851+alben099@users.noreply.github.com>
2021-12-29 18:00:24 -0700

    Add files via upload

---
+++ Catch Ball Source Code/ObjectPools.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The script that creates pools of objects for reusability and performance enhancement
+/// </summary>
+public class ObjectPools : MonoBehaviour
+{
+    /// <summary>
+    /// The pool class that has a number of objects organized with a tag
+    /// </summary>
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    #region Singleton
+    public static ObjectPools instance;
+
+    public static ObjectPools Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.LogError("The GameManager is NULL.");
+            }
+
+            return instance;
+        }
+    }
+
+    /// <summary>
+    /// Creates the instance of this object as a singleton
+    /// </summary>
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    // Public variables for getting object pools in a dictionary
+    public List<Pool> pools;
+    public Dictionary<string, List<GameObject>> poolDictionary;
+
+    /// <summary>
+    /// Start at frame one to make a dictionary of object pools that have
+    /// instantiated a specified number of objects
+    /// </summary>
+    void Start()
+    {
+        poolDictionary = new Dictionary<string, List<GameObject>>();
+        foreach (Pool pool in pools)
+        {
+            List<GameObject> objectPool = new List<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Add(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    /// <summary>
+    /// Grabs an inactive object from a specified pool
+    /// </summary>
+    /// <param name="tag">The pool's name</param>
+    /// <returns>An activated object from the pool</returns>
+    public GameObject SpawnFromPool(string tag)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with tag " + tag + " doesn't exist.");
+            return null;
+        }
+
+        /*GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+        poolDictionary[tag].Enqueue(objectToSpawn);*/
+
+        foreach (GameObject obj in poolDictionary[tag])
+        {
+            if (!obj.activeInHierarchy)
+            {
+                obj.SetActive(true);
+                return obj;
+            }
+        }
+        return null;
+    }
+}
